3d69d71e5e91d155065f9bb57b18386119d9a57a
Humberto Burgos <hjunior_04@hotmail.com>
2018-12-04 22:24:03 -0400

    Save

--- HistorialMedico/Controllers/AsistenteController.cs
+++ HistorialMedico/Controllers/AsistenteController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using HistorialMedico.Models;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 
 namespace HistorialMedico.Controllers
 {
@@ -163,5 +167,110 @@ namespace HistorialMedico.Controllers
         {
             return View();
         }
+
+        public ActionResult VerificarCedula()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult VerificarCedula(string cedulaP)
+        {
+            paciente paciente = db.paciente.Where(e => e.cedula == cedulaP).FirstOrDefault();
+
+            if (paciente != null)
+            {
+                return PacienteEncontrado(cedulaP);
+            }
+            else
+            {
+                return View("CrearPaciente");
+            }
+
+
+            return View();
+        }
+
+        public ActionResult PacienteEncontrado(string cedula)
+        {
+            paciente paciente = db.paciente.Where(e => e.cedula == cedula).FirstOrDefault();
+            List<paciente> pacientes = new List<paciente>();
+            pacientes.Add(paciente);
+            return View("PacienteEncontrado", pacientes.ToList());
+        }
+
+
+        public ActionResult PagoDeConsulta()
+        {
+            var getname = db.paciente.ToList();
+            SelectList list = new SelectList(getname, "id_paciente", "nombre");
+            ViewBag.NombrePaciente = list;
+            var getnameDoctor = db.precioConsulta.ToList();
+            SelectList lista = new SelectList(getnameDoctor, "id_precioConsulta", "costo");
+            ViewBag.NombreDoctor = lista;
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult PagoDeConsulta([Bind(Include = "id_Paciente,id_PrecioConsulta,pago,total,fecha")] pagoConsulta pagoConsulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pagoConsulta.Add(pagoConsulta);
+                db.SaveChanges();
+
+            }
+            Document pdfDoc = new Document(PageSize.A4, 10, 10, 10, 10);
+
+            try
+            {
+                PdfWriter.GetInstance(pdfDoc, System.Web.HttpContext.Current.Response.OutputStream);
+
+                pdfDoc.Open();
+                string cadenaFinal = "";
+                paciente asistente = db.paciente.Find(pagoConsulta.id_Paciente);
+                precioConsulta precioConsulta = db.precioConsulta.Find(pagoConsulta.id_PrecioConsulta);
+                cadenaFinal += "<br><center><h2>Factura</h2></center><br><br><div>" +
+                    "<h4> Cedula:  " + asistente.cedula + "</h4><br>" +
+                    "<h4> Nombre:  " + asistente.nombre + "</h4><br>" +
+                    "<h4> Telefono:  " + asistente.telefono + "</h4><br>" +
+                    "<h4> Telefono:  " + asistente.correo + "</h4><br>" +
+                    "<h4> Costo:  " + precioConsulta.costo + "</h4><br>" +
+                    "<h4> Pago:  " + pagoConsulta.pago + "</h4><br>" +
+                    "<h4> Total:  " + pagoConsulta.total + "</h4><br>" +
+                    "<h4> Fecha: " + pagoConsulta.fecha + "</h4>" + "</div>";
+
+                string strContent = cadenaFinal;
+
+                var parsedHtmlElements = HTMLWorker.ParseToList(new StringReader(strContent), null);
+
+                foreach (var htmlElement in parsedHtmlElements)
+                {
+                    pdfDoc.Add(htmlElement as IElement);
+                }
+
+                pdfDoc.Close();
+                Response.ContentType = "application/pdf";
+
+                Response.AddHeader("content-disposition", "attachment; filename=Receta.pdf");
+                System.Web.HttpContext.Current.Response.Write(pdfDoc);
+                Response.Flush();
+                Response.End();
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+
+            return View(pagoConsulta);
+
+        }
+
     }
+
 }
